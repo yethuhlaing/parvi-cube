be3dc0bafce72535826edfc10c36eec92d281172
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-09-21 17:36:45 +0300

    Add Comma in FIle Import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { About } from './components/about';
 import Countdown from './components/countdown';
 import { FAQ } from './components/faq';
-import Footer from "./components/footer"
+import Footer from "./components/footer";
 import Hero from './components/hero';
 
 export default function Home() {
