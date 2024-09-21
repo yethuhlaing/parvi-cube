45896b44c303db9ed16e0e6c1f53bc43f59412c3
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-09-21 17:54:15 +0300

    Change FIle Name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { About } from './components/about';
 import Countdown from './components/countdown';
 import { FAQ } from './components/faq';
-import Footer from "./components/footer";
+import FeatureSection from './components/FeatureSection';
 import Hero from './components/hero';
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       <About />
       {/* <CallToAction /> */}
       <FAQ />
-      <Footer />
+      <FeatureSection />
     </main>
   )
 }
